494bbcca22da408a46f36157ce5718216c768c90
niicaa17 <nandachairunnis17@gmail.com>
2026-03-05 17:42:12 +0700

    update

--- 1.py
+++ 1.py
@@ -2,3 +2,9 @@ x = 4
 x_squared = tf.square(x)
  
 print("tanganku {}".format(x_squared))
+
+tensor1 = tf.constant([[1, 2, 3], [4, 5, 6]])
+tensor2 = tf.constant([1, 2, 3])
+ 
+result = tf.add(tensor1, tensor2)
+print(result)
